58234f754d8c7eeab298027b7f6e6a4cdf85965c
NandoTheessen <nando.theessen@gmail.com>
2018-06-29 16:29:24 +0200

    debugging messages for put route

--- routes/notes.js
+++ routes/notes.js
@@ -20,7 +20,7 @@ router
     })
     .put(authenticated, (req, res) => {
         const { title, body, id } = req.body
-        if (title === unedfined || body === unedfined || id === undefined) res.status(422).json({ error: 'Please include all needed fields' })
+        if (title === undefined || body === undefined || id === undefined) res.status(422).json({ error: 'Please include all needed fields' })
 
         const update = { title: title, body: body }
 
