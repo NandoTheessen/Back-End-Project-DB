fa7a43a41c6b82d426191916a0b92f47a4dc2e7d
NandoTheessen <nando.theessen@gmail.com>
2018-07-05 13:06:02 +0200

    Added an error message for missing user

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,7 @@ const mySecret = process.env.mysecret || 'aosdjfoasdjif'
 router.post('/', (req, res) => {
     const { password, username } = req.body
     password === undefined || username === undefined ? res.status(400).json({ Error: 'Please provide a password and a username' }) : null
-
+    console.log('firing');
     User.findOne({ username })
         .populate('notes', { _id: 0, title: 1, body: 1 })
         .then((user, err) => {
@@ -26,6 +26,8 @@ router.post('/', (req, res) => {
                         }
                     })
                     .catch(err => res.status(500).json(err))
+            } else {
+                res.status(400).json({ error: 'no such user!' })
             }
         })
         .catch(err => res.status(500))
